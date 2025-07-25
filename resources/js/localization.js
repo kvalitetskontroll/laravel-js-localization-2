e199d9bbe608e1f62b7461007443ae4e79fd8532
stelar7 <dudedbz@gmail.com>
2025-07-25 10:37:19 +0200

    fix choice

--- resources/js/localization.js
+++ resources/js/localization.js
@@ -5,7 +5,6 @@
     var intervalRegexp = /^({\s*(\-?\d+(\.\d+)?[\s*,\s*\-?\d+(\.\d+)?]*)\s*})|([\[\]])\s*(-Inf|\*|\-?\d+(\.\d+)?)\s*,\s*(\+?Inf|\*|\-?\d+(\.\d+)?)\s*([\[\]])$/;
     var anyIntervalRegexp = /({\s*(\-?\d+(\.\d+)?[\s*,\s*\-?\d+(\.\d+)?]*)\s*})|([\[\]])\s*(-Inf|\*|\-?\d+(\.\d+)?)\s*,\s*(\+?Inf|\*|\-?\d+(\.\d+)?)\s*([\[\]])/;
 
-
     /* Utility functions: */
 
     /**
@@ -17,39 +16,48 @@
      * @param {Object} replacements Associative array: { variableName: "replacement", ... }
      * @return {String} The input message with all replacements applied.
      */
-    var applyReplacements = function (message, replacements) {
+    var applyReplacements = function(message, replacements) {
         for (var replacementName in replacements) {
             var replacement = String(replacements[replacementName]);
 
             // 'welcome' => 'Welcome, :name' => 'Welcome, dayle'
             message = message.replace(
                 new RegExp(':' + replacementName, 'g'),
-                replacement
+                replacement,
             );
             // 'welcome' => 'Welcome, :NAME' => 'Welcome, DAYLE'
             message = message.replace(
                 new RegExp(':' + replacementName.toUpperCase(), 'g'),
-                replacement.toUpperCase()
+                replacement.toUpperCase(),
             );
             // 'welcome' => 'Welcome, :Name' => 'Welcome, Dayle'
             message = message.replace(
                 new RegExp(':' + (replacementName.charAt(0).toUpperCase() + replacementName.substr(1)), 'g'),
-                replacement.charAt(0).toUpperCase() + replacement.substr(1)
+                replacement.charAt(0).toUpperCase() + replacement.substr(1),
             );
         }
 
         return message;
     };
 
-    var isEmpty = function (obj) {
-        for(var prop in obj) {
-            if(obj.hasOwnProperty(prop))
+    var isEmpty = function(obj) {
+        for (var prop in obj) {
+            if (Object.hasOwnProperty.call(obj, prop))
                 return false;
         }
 
         return true;
     };
 
+    var convertNumber = function(str) {
+        if (str === '-Inf') {
+            return -Infinity;
+        } else if (str === '+Inf' || str === 'Inf' || str === '*') {
+            return Infinity;
+        }
+
+        return parseInt(str, 10);
+    };
 
     /* Lang: */
 
@@ -69,22 +77,27 @@
          * @param {Object} [replacements]   Associative array: { variableName: "replacement", ... }
          * @return {String} Translated message.
          */
-        get : function(messageKey, replacements, forceLocale) {
+        get: function(messageKey, replacements, forceLocale) {
             var uselocale = locale;
+
             if (forceLocale) {
                 uselocale = forceLocale;
             }
-            if (typeof messages[uselocale][messageKey] == "undefined") {
+
+            if (typeof messages[uselocale][messageKey] == 'undefined') {
                 /* like Lang::get(), if messageKey is the name of a lang file, return it as an array */
                 var result = {};
+
                 for (var prop in messages[uselocale]) {
                     if (prop.indexOf(messageKey + '.') > -1) {
                         result[prop] = messages[uselocale][prop];
                     }
                 };
+
                 if (!isEmpty(result)) {
                     return result;
                 }
+
                 /* if there is nothing to return, return messageKey */
                 return messageKey;
             }
@@ -106,11 +119,11 @@
          * @param {String} messageKey   Message key.
          * @return {Boolean} True if the given message exists.
          */
-        has : function(messageKey) {
-            return typeof messages[locale][messageKey] != "undefined";
+        has: function(messageKey) {
+            return typeof messages[locale][messageKey] != 'undefined';
         },
 
-        _testInterval = function(count, interval) {
+        _testInterval: function(count, interval) {
             if (typeof interval !== 'string') {
                 throw 'Invalid interval: should be a string.';
             }
@@ -118,12 +131,14 @@
             interval = interval.trim();
 
             var matches = interval.match(intervalRegexp);
+
             if (!matches) {
                 throw 'Invalid interval: ' + interval;
             }
 
             if (matches[2]) {
                 var items = matches[2].split(',');
+
                 for (var i = 0; i < items.length; i++) {
                     if (parseInt(items[i], 10) === count) {
                         return true;
@@ -137,9 +152,11 @@
 
                 var leftDelimiter = matches[1];
                 var leftNumber = convertNumber(matches[2]);
+
                 if (leftNumber === Infinity) {
                     leftNumber = -Infinity;
                 }
+
                 var rightNumber = convertNumber(matches[3]);
                 var rightDelimiter = matches[4];
 
@@ -164,13 +181,13 @@
          * @param {Object} [replacements]   Associative array: { variableName: "replacement", ... }
          * @return {String} Translated message.
          */
-        choice : function(messageKey, count, replacements) {
-            if (typeof messages[locale][messageKey] == "undefined") {
+        choice: function(messageKey, count, replacements) {
+            if (typeof messages[locale][messageKey] == 'undefined') {
                 return messageKey;
             }
 
-            var message;
-            var messageSplitted = messages[locale][messageKey].split('|');
+            var message = messages[locale][messageKey];
+            var messageParts = message.split('|');
 
             // Get the explicit rules, If any
             var explicitRules = [];
@@ -188,17 +205,17 @@
             // Check if there's only one message
             if (messageParts.length === 1) {
                 // Nothing to do here
-                return message;
+                return applyReplacements(message, replacements);
             }
 
             // Check the explicit rules
             for (var j = 0; j < explicitRules.length; j++) {
-                if (this._testInterval(number, explicitRules[j])) {
-                    return messageParts[j];
+                if (this._testInterval(count, explicitRules[j])) {
+                    return applyReplacements(messageParts[j], replacements);
                 }
             }
 
-            return messageParts[count == 1 ? 0 : 1];
+            return applyReplacements(messageParts[count == 1 ? 0 : 1], replacements);
         },
 
         /**
@@ -210,13 +227,13 @@
          * @param {String} localeId The locale returned by Laravel's Lang::locale().
          * @throws {Error} An error is thrown if messages[localeId] is not defined.
          */
-        setLocale : function(localeId) {
+        setLocale: function(localeId) {
             locale = localeId;
 
             if (!messages[localeId]) {
                 throw new Error(
-                    'No messages defined for locale: "' + localeId + '". ' +
-                    'Did you forget to enable it in the configuration?'
+                    'No messages defined for locale: "' + localeId + '". '
+                    + 'Did you forget to enable it in the configuration?',
                 );
             }
         },
@@ -228,7 +245,7 @@
          * @static
          * @return {String} The current locale.
          */
-        locale : function() {
+        locale: function() {
             return locale;
         },
 
@@ -240,17 +257,16 @@
          * @static
          * @param {Object} _messages  An associative array: { messageKey: "message", ... }
          */
-        addMessages : function(_messages) {
+        addMessages: function(_messages) {
             for (var key in _messages) {
                 messages[key] = _messages[key];
             }
-        }
+        },
     };
-
 
     /* Export: */
 
-    this.Lang   = Lang;
-    this.trans  = Lang.get;
+    this.Lang = Lang;
+    this.trans = Lang.get;
     this.transChoice = Lang.choice;
 })();
